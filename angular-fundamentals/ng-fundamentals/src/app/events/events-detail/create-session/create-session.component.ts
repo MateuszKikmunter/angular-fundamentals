8a812108cc1f630497351e708f4bdfd21afcab11
m.kikmunter <M.Kikmunter@gmail.com>
2018-09-02 19:57:04 +0100

    wired up controls on create session form

--- angular-fundamentals/ng-fundamentals/src/app/events/events-detail/create-session/create-session.component.ts
+++ angular-fundamentals/ng-fundamentals/src/app/events/events-detail/create-session/create-session.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '../../../../../node_modules/@angular/forms';
 
 @Component({
   selector: 'app-create-session',
@@ -7,9 +8,33 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CreateSessionComponent implements OnInit {
 
+  newSessionForm: FormGroup;
+  name: FormControl;
+  presenter: FormControl;
+  duration: FormControl;
+  level: FormControl;
+  abstract: FormControl;
+
   constructor() { }
 
   ngOnInit() {
+    this.name = new FormControl('', [Validators.required]);
+    this.presenter = new FormControl('', [Validators.required]);
+    this.duration = new FormControl('', [Validators.required]);
+    this.level = new FormControl('', [Validators.required]);
+    this.abstract = new FormControl('', [Validators.required, Validators.maxLength(400)]);
+
+    this.newSessionForm = new FormGroup({
+      name: this.name,
+      presenter: this.presenter,
+      duration: this.duration,
+      level: this.level,
+      abstract: this.abstract
+    })
+  }
+
+  saveSession(newSessionFormValues) : void{
+    console.log(newSessionFormValues);
   }
 
 }
