f6691a9973558b524e2374c11f999262b2ac2ac2
Mateusz Kikmunter <mateusz.kikmunter@willistowerswatson.com>
2018-09-10 16:17:11 +0100

    started to use http proxy server for serving data

--- angular-fundamentals/ng-fundamentals/src/app/app.module.ts
+++ angular-fundamentals/ng-fundamentals/src/app/app.module.ts
@@ -2,6 +2,7 @@ import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 import {
@@ -60,7 +61,8 @@ let jQuery = window['$'];
     BrowserModule,
     FormsModule,
     ReactiveFormsModule,
-    RouterModule.forRoot(appRoutes)
+    RouterModule.forRoot(appRoutes),
+    HttpClientModule
   ],
   providers: [
     EventService,
