0c8b915bd6e158b08f44209c83e13cb874741f17
Mateusz Kikmunter <mateusz.kikmunter@willistowerswatson.com>
2018-09-13 10:49:04 +0100

    added first intergrated test for session list component

--- angular-fundamentals/ng-fundamentals/src/app/events/events-detail/session-list/session-list.component.integrated.spec.ts
+++ angular-fundamentals/ng-fundamentals/src/app/events/events-detail/session-list/session-list.component.integrated.spec.ts
@@ -1,3 +1,6 @@
+import { EventService } from './../../shared/event.service';
+import { CollapsibleWellComponent } from './../../../common/collapsible-well/collapsible-well.component';
+import { UpvoteComponent } from './../upvote/upvote.component';
 import { TestBed, async, ComponentFixture } from "@angular/core/testing";
 import { DebugElement } from "@angular/core";
 import { By } from '@angular/platform-browser';
@@ -6,6 +9,7 @@ import { ISession } from './../../shared/event.session';
 import { AuthService } from './../../../user/auth.service';
 import { SessionListComponent } from "./session-list.component";
 import { VoterService } from '../voter.service';
+import { DurationPipe } from '../../shared/duration.pipe.';
 
 describe("SessionListComponent", () => {
     let fixture: ComponentFixture<SessionListComponent>,
@@ -13,25 +17,56 @@ describe("SessionListComponent", () => {
         element: HTMLElement,
         debugEl: DebugElement;
 
-        beforeEach(async(() => {
-            let mockAuthService = {};
-            let mockVoterService = {};
+    beforeEach(async(() => {
+        let mockAuthService = {
+            isAuthenticated: () => true,
+            currentUser: { username: "" }
+        };
 
-            TestBed.configureTestingModule({
-                imports: [],
-                declarations: [SessionListComponent],
-                providers: [
-                    { provide: AuthService, useValue: mockAuthService },
-                    { provide: VoterService, useValue: mockVoterService }
-                ],
-                schemas: []
-            });
-        }));
+        let mockVoterService = {
+            userHasVoted: () => true
+        };
 
-        beforeEach(() => {
-            fixture = TestBed.createComponent(SessionListComponent);
-            component = fixture.componentInstance;
-            debugEl = fixture.debugElement;
-            element = fixture.nativeElement;
+        let mockEventService = {};
+
+        TestBed.configureTestingModule({
+            imports: [],
+            declarations: [SessionListComponent, UpvoteComponent, DurationPipe, CollapsibleWellComponent],
+            providers: [
+                { provide: AuthService, useValue: mockAuthService },
+                { provide: VoterService, useValue: mockVoterService },
+                { provide: EventService, useValue: mockEventService }
+            ],
+            schemas: []
         });
+    }));
+
+    beforeEach(() => {
+        fixture = TestBed.createComponent(SessionListComponent);
+        component = fixture.componentInstance;
+        debugEl = fixture.debugElement;
+        element = fixture.nativeElement;
+    });
+
+    describe("initial display", () => {
+        it("should have the correct session title", () => {
+            component.sessions = [{
+                id: 3,
+                name: "Session 1",
+                presenter: "Joe",
+                duration: 1,
+                level: "beginner",
+                abstract: "abstract",
+                voters: ["John", "Bob"]
+            }];
+            component.filterBy = "all";
+            component.sortBy = "name";
+            component.eventId = 4;
+
+            component.ngOnChanges();
+            fixture.detectChanges();
+
+            expect(element.querySelector("[well-title]").textContent).toContain("Session 1");
+        });
+    });
 });
